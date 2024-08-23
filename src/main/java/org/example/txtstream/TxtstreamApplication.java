a67521e33bb321abd0e21ad524bfecf2e421d9ad
Heeneth-sai <heeneth.sai@dragarwal.com>
2024-08-23 10:21:06 +0530

    Created a new Txtstream project-be

---
+++ src/main/java/org/example/txtstream/TxtstreamApplication.java
@@ -0,0 +1,13 @@
+package org.example.txtstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/txtstream/TxtstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.txtstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxtstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
